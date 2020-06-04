e5eaf47521486800997829bc81cc7541e36abeb0
github username <github email address>
2020-06-03 22:35:18 -0700

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,53 @@
+require 'pry'
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status 
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction
+  
+    if @sender.balance > @amount && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else 
+      @status = "rejected"
+      return "Transaction  rejected. Please check your account balance."
+    end
+  end
+
+  # def execute_transaction
+  #   if @sender.balance < @amount
+  #     self.status = "rejected"
+  #     return "Transaction rejected. Please check your account balance."
+
+  #   elsif @status == "complete"
+  #     puts "Transaction was already excuted"
+  #   else
+  #     @sender.deposit( @amount * -1 ) 
+  #     @receiver.deposit( @amount )
+  #     @status = "complete"
+  #   end
+  # end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
+  end
 end
